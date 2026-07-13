2752cedcab5919e9677ebdc7dceec57b30309282
rijophilip47 <rijophilip2@gmail.com>
2026-07-13 21:48:26 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello DevOps</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Hello DevOps 🚀</h1>
+
+<p>My first DevOps project.</p>
+
+</body>
+</html>
